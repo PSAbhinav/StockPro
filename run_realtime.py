d7b4720d8c8754cbd6b7ce0ffa1c66ff39cc4658
P S Abhinav Krishna <abhinavpemmaraju@gmail.com>
2026-03-23 15:19:11 +0530

    Add Render deployment configuration

--- run_realtime.py
+++ run_realtime.py
@@ -70,8 +70,8 @@ def main():
         sys.exit(1)
     
     # Configuration
-    host = '127.0.0.1'
-    port = 5000
+    host = os.environ.get('HOST', '0.0.0.0' if 'RENDER' in os.environ else '127.0.0.1')
+    port = int(os.environ.get('PORT', 5000))
     url = f'http://{host}:{port}'
     
     print(f"Starting server on {url}")
